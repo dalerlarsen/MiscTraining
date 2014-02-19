24df9c99b7afa0fa867e31fecff6ee3b793c8ac6
dalerlarsen <dalerlarsen@gmail.com>
2014-02-19 10:30:50 -0500

    Adding rest of SpringFundamental projects

---
+++ SpringFundamentals/spring_sample_java/src/AppConfig.java
@@ -0,0 +1,42 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import com.pluralsight.service.CustomerService;
+import com.pluralsight.service.CustomerServiceImpl;
+
+
+
+@Configuration
+@ComponentScan({"com.pluralsight"})
+@PropertySource("app.properties") // com/pluralsight/model/app.properties ..whereever you like
+public class AppConfig {
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	
+	@Bean(name = "customerService")
+	@Scope("prototype")
+	public CustomerService getCustomerService() {
+//		CustomerServiceImpl customerService = new CustomerServiceImpl();
+//		customerService.setCustomerRepository(getCustomerRepository());
+
+//		CustomerServiceImpl customerService = new CustomerServiceImpl(getCustomerRepository());
+
+		CustomerServiceImpl customerService = new CustomerServiceImpl();
+
+		return customerService;
+	}
+	
+	
+//	@Bean(name = "customerRepository")
+//	public CustomerRepository getCustomerRepository() {
+//		return new HibernateCustomerRepositoryImpl();
+//	}
+
+}
