d12cc2829e0863e3616d7fd0c28f667e8392b419
dalerlarsen <dalerlarsen@gmail.com>
2014-02-26 15:28:54 -0500

    More changes...added in ajaxMocking

--- JavaScript/JavaScriptUnitTesting/FromQunitSlideShow/myappDomTesting-test.js
+++ JavaScript/JavaScriptUnitTesting/FromQunitSlideShow/myappDomTesting-test.js
@@ -125,3 +125,35 @@ test("stops & starts", function() {
 	    start();
 	  });
 });
+
+// Mocking AJAX - if you mock your AJAX requests, you actually test your JavaScript and not your server
+
+// Simulate your API.
+$.mockAjax("json", {
+    "/user": {status: -1},
+    "/user/(\\d+)": function(matches) {
+        return {status: 1, user: "sample user " + matches[1]};
+    }
+});
+
+// Unit tests.
+
+test("user tests", function() {
+    expect(5);
+
+    stop();
+    $.getJSON("/user", function(data) {
+        ok(data, "data is returned from the server");
+        equal(data.status, "-1", "no user specified, status should be -1");
+        start();
+    });
+
+    stop();
+    //noinspection JSUnresolvedFunction
+    $.getJSON("/user/123", function(data) {
+        ok(data, "data is returned from the server");
+        equal(data.status, "1", "user found, status should be 1");
+        equal(data.user, "sample user 123", "user found, id should be 123");
+        start();
+    });
+});
